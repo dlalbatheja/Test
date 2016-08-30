d2421a93dbd114950c5b74a38dd770b40e22789f
dlalbatheja <ramdanish@gmail.com>
2016-08-31 03:00:42 +1000

    Add files via upload

---
+++ SortStudentsData/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeScores
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            string inputFileName = string.Empty;
+
+            #if !DEBUG
+
+            if (args != null && args.Length >= 1)
+            {
+                inputFileName = args[0];
+                SortData(inputFileName);
+            }
+            else
+            {
+                Exit("Program expects one parameter. Parameter is missing!");
+            }
+
+            #else
+
+            //Unit Test Case A - Valid File - Different Scores
+            inputFileName = Environment.CurrentDirectory + @"\UnitTestData\ClassA.txt";
+            SortData(inputFileName);
+            
+            //Unit Test Case A - Valid File - Same Scores
+            inputFileName = Environment.CurrentDirectory + @"\UnitTestData\ClassB.txt";
+            SortData(inputFileName);
+
+            //Unit Test Case B - InValid File
+            inputFileName = Environment.CurrentDirectory + @"\UnitTestData\ClassB1.txt";
+            SortData(inputFileName);
+
+            #endif
+        }
+
+        private static void SortData(string inputFileName)
+        {
+            FileInfo inputFileInfo;
+
+            if ((inputFileInfo = Utility.GetFileInfo(inputFileName)) != null)
+            {
+                StudentsData.GetFromFile(inputFileInfo);
+                StudentsData.Display();
+                StudentsData.SortRecords();
+                string outputFileName = StudentsData.WriteToFile();
+
+                Console.WriteLine("Finished: created {0}", outputFileName.GetFileName());
+                Console.Read();
+            }
+            else
+            {
+                Exit("Input file is not valid. Please check if it exists or it is accessible!");
+            }
+        }
+
+        static void Exit(string messgae)
+        {
+            Console.WriteLine(messgae);
+            Console.Write("Hit Any Key to Exit...");
+            Console.Read();
+        }
+    }
+}
---
+++ SortStudentsData/Utility.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeScores
+{
+    static class Utility
+    {
+        public static FileInfo GetFileInfo(string fileName)
+        {
+            FileInfo fileInfo = new FileInfo(fileName);
+
+            if (fileInfo.Exists)
+                return fileInfo;
+            else
+                return null;
+        }
+
+        public static string RemoveExtension(this string fileName)
+        {
+            int extIndex = fileName.LastIndexOf('.');
+
+            if(extIndex > 0)
+            {
+                return fileName.Substring(0, extIndex);
+            }
+
+            return fileName;
+        }
+
+        public static string GetFileName(this string fileName)
+        {
+            int extIndex = fileName.LastIndexOf('\\');
+
+            if (extIndex > 0)
+            {
+                return fileName.Substring(extIndex+1, fileName.Length - extIndex - 1);
+            }
+
+            return fileName;
+        }
+    }
+}
